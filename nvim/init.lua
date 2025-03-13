b30e0382796a6ae2b51d44367fb50c222b989c18
gc79mh <wiktor.bochenski@student.put.poznan.pl>
2025-03-13 18:13:45 +0100

    essa

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,3 +12,5 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
 
+require("config.lazy")
+require("hardtime").setup()
