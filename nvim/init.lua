5f6ca71d117faa9cb12650091f9d57807bd57457
gc79mh <wiktor.bochenski@student.put.poznan.pl>
2025-04-03 15:38:58 +0200

    Added Hyprland , nvim , kitty , lxc , waybar

---
+++ nvim/init.lua
@@ -0,0 +1,67 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		ensure_installed = { "python" },
+		highlight = { enable = true },  
+		indent = { enable = true },
+		build = ":TSUpdate"
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {}
+	},
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+})
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "python" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Use the system clipboard
+vim.opt.clipboard = 'unnamedplus'  
+
+-- Disable the statusline
+vim.opt.laststatus = 0
+
+-- Enable syntax highlighting
+-- vim.cmd('syntax off')
+vim.cmd("colorscheme tokyonight")
+
+-- Transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
